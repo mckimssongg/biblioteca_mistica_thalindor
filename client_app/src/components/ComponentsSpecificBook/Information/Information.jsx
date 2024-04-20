57316c7f2a1993d24f0d8bc522ef95f39a3906d1
DulceHerrera07 <hgdulce267@gmail.com>
2024-04-20 13:46:54 -0600

    Maquetado de view SpecificBook Coraline

--- client_app/src/components/ComponentsSpecificBook/Information/Information.jsx
+++ client_app/src/components/ComponentsSpecificBook/Information/Information.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { EyeIcon } from '../EyeIcon/EyeIcon';
 
 export default function Information({n_pag, editorial, idiomas, fecha_publicacion, className}) {
   return (
+    
+
     <div className={className}>
         <p><strong>No. pág: </strong>{n_pag}</p>
         <p><strong>Editorial: </strong>{editorial}</p>
         <p><strong>Idiomas: </strong>{idiomas}</p>
         <p><strong>Fecha de publicación: </strong>{fecha_publicacion}</p>
+
+        <EyeIcon initialActive={false} />
     </div>
   )
 }
--- client_app/src/views/SpecificBook/SpecificBookCaroline.jsx
+++ client_app/src/views/SpecificBook/SpecificBookCaroline.jsx
@@ -15,7 +15,7 @@ export default function SpecificBookCaroline() {
    <>
       <Navar/>
       <div className='container'>
-        <div className="grid-container">
+        <div className="grid-container1">
           <Card
             className={"card link center"}
             icon={<ISBN/>}
@@ -23,9 +23,12 @@ export default function SpecificBookCaroline() {
             bodyText='Autor: Neil Gaiman'
             redirectUrl='https://corazondeartista.wordpress.com/wp-content/uploads/2014/09/gaimanneil-coraline.pdf'
           />
+        </div>  
+
+        <div className='grid-container2'>
           <Description
             className={"content-description"}
-            description='Coraline Jones y sus padres "workaholics" se mudan a una vieja casa que ha sido dividida en distintos pisos de departamentos. Entre los inquilinos están las señoritas Spink y Forcible, dos mujeres ancianas retiradas de la farándula del teatro, y el Señor Bob, inicialmente referido a él como "el viejo loco de arriba”, que se define como entrenador de un circo de ratones. El piso junto al de Coraline está deshabitado, y una puerta enlaza ambos departamentos, pero esta está bloqueada por ladrillos una vez abierta.'
+            description='Coraline Jones y sus padres "workaholics" se mudan a una vieja casa que ha sido dividida en distintos pisos de departamentos. Entre los inquilinos están las señoritas Spink y Forcible, dos mujeres ancianas retiradas de la farándula del teatro, y el Señor Bob, inicialmente referido a él como "el viejo loco de arriba”, que se define como entrenador de un circo de ratones. El piso junto al de Coraline está deshabitado, y una puerta enlaza ambos departamentos, pero está bloqueada por ladrillos una vez abierta.'
           />       
 
           <Gender
